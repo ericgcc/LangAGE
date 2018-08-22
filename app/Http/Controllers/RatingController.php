4eb07219155aa9fbc40baf5ce7539ebd75b96bd2
Eric Gcc <ericgcc@gmail.com>
2018-08-22 14:17:42 -0500

    [core>change]Index and Store methods
    The index method verifies if the user has already finished the
    evaluation, if so, shows the corresponding message page.
    
    Store method modifies finished field in the users table when
    submiting the form, to indicate that a user has finished.

--- app/Http/Controllers/RatingController.php
+++ app/Http/Controllers/RatingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Rating;
@@ -10,32 +11,44 @@ use Illuminate\Support\Facades\DB;
 class RatingController extends Controller
 {
 
-    public function index(){
+    public function index()
+    {
+        $status = User::where('id', Auth::id())
+                    ->pluck('finished') // this return a Collection (array)
+                    ->first(); // this return the single value
+
+        if($status) return view('pages.finished');
+
 
         $sliders = Rating::where('user_id', Auth::id())
-                            ->orderBy('track_id')
-                            ->orderBy('question_id')
-                            ->pluck('rating');
+            ->orderBy('track_id')
+            ->orderBy('question_id')
+            ->pluck('rating');
 
-        return view('index', ['sliders' => $sliders]);
+        return view('pages.index', ['sliders' => $sliders]);
     }
 
-    public function store(Request $request){
-        $ratings =  $request->input("ratings");
-        $number_of_questions =  $request->input("number_of_questions");
-        $number_of_tracks =  $request->input("number_of_tracks");
+    public function store(Request $request)
+    {
+        $ratings = $request->input("ratings");
+        $number_of_questions = $request->input("number_of_questions");
+        $number_of_tracks = $request->input("number_of_tracks");
+        $finished = $request->input("finished") === 'true' ? true:false;
 
-        for($t = 0; $t < $number_of_tracks; $t++)
-            for ($q = 0; $q < $number_of_questions; $q++)
-                try {
+        try {
+            for ($t = 0; $t < $number_of_tracks; $t++)
+                for ($q = 0; $q < $number_of_questions; $q++)
+
                     DB::insert(DB::raw("INSERT INTO ratings values(:user_id, :question_id, :track_id, :rating)
                                   ON DUPLICATE KEY UPDATE rating = :new_rating"),
                         ['user_id' => Auth::id(), 'question_id' => $q + 1, 'track_id' => $t + 1,
                             'rating' => $ratings[$t][$q] == "" ? NULL : floatval($ratings[$t][$q]),
                             'new_rating' => $ratings[$t][$q] == "" ? NULL : floatval($ratings[$t][$q])]);
-                }catch(\Exception $e){
-                    return response()->json("The data could not be saved, try again.");
-                }
+
+            if ($finished) User::where('id', Auth::id())->update(['finished' => 1]);
+        } catch (\Exception $e) {
+            return response()->json("The data could not be saved, try again.");
+        }
         return response()->json("true");
     }
 }
