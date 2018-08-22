8d917845f239b1226159b5eaf9e5979d7c866355
Eric Gcc <ericgcc@gmail.com>
2018-08-22 14:14:32 -0500

    [core>add]User Model to query the database for users

---
+++ app/Models/User.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EGarciaCanoCa
+ * Date: 22/08/2018
+ * Time: 12:40 PM
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    public $timestamps = false;
+}
