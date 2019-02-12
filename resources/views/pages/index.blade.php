61c40d51e8e4cb2c3f430af3cc53a260f9ab7c17
Eric Gcc <ericgcc@gmail.com>
2019-02-12 14:24:12 -0600

    [style>change] Updating rating page
    The rating page was updated, using the new master layout and blade,
    the laravel tamplating system.
    
    We also added the necessary plugins for the functionalities of
    the page.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,1 +1,34 @@
 @extends('layouts.master')
+
+@section('vendor-styles')
+    {!! Html::style("app-assets/vendors/css/nouislider/nouislider.min.css") !!}
+    {!! Html::style("app-assets/vendors/css/UbaPlayer/ubaplayer.css") !!}
+    {!! Html::style("app-assets/vendors/css/webui-popover/jquery.webui-popover.min.css") !!}
+    {!! Html::style("app-assets/vendors/css/hopscotch/hopscotch.css") !!}
+    {!! Html::style("app-assets/vendors/css/nouislider/noui-slider.css") !!}
+    {!! Html::style("app-assets/vendors/css/nouislider/palette-noui.css") !!}
+    {!! Html::style("app-assets/vendors/css/sweetalert2/sweetalert2.min.css") !!}
+@endsection
+
+@section('admin-navs')
+    @if ($is_admin === 1)
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('download')}}">
+                <i class="icon-cloud-download"></i>
+                <span>Télécharger résultats</span>
+            </a>
+        </li>
+    @endif
+@endsection
+
+@section('vendor-scripts')
+    {!! Html::script("app-assets/vendors/js/jquery/jquery.sticky.js") !!}
+    {!! Html::script("app-assets/vendors/js/wNumb/wNumb.js") !!}
+    {!! Html::script("app-assets/vendors/js/nouislider/nouislider.min.js") !!}
+    {!! Html::script("app-assets/vendors/js/UbaPlayer/jquery.ubaplayer.min.js") !!}
+    {!! Html::script("app-assets/vendors/js/webui-popover/jquery.webui-popover.min.js") !!}
+    {!! Html::script("app-assets/vendors/js/sweetalert2/sweetalert2.min.js") !!}
+    {!! Html::script("app-assets/vendors/js/hopscotch/hopscotch.min.js") !!}
+    {!! Html::script("app-assets/vendors/js/app/detectmobilebrowser.js") !!}
+    {!! Html::script("app-assets/vendors/js/jquery.matchHeight/jquery.matchHeight.min.js") !!}
+@endsection
